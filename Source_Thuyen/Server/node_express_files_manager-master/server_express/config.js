dd8952afcf2102e344b921feee6c75df8142ca9e
truongtechno <truongnhthtb@gmail.com>
2016-04-27 23:45:56 +0700

    casc

---
+++ Source_Thuyen/Server/node_express_files_manager-master/server_express/config.js
@@ -0,0 +1,22 @@
+var config = {};
+
+config.status = {};
+config.redis = {};
+config.web = {};
+config.firebase = {};
+
+config.redis.host = 'hostname';
+config.redis.port = 6379;
+config.web.port = process.env.WEB_PORT || 9980;
+config.firebase.host = 'https://files-manager.firebaseio.com/'
+
+config.database = '/home/truongtechno/node_express_files_manager-master/server_express/public/files/';
+
+config.status.ok = 'ok'
+config.status.ng = 'ng'
+config.status.no_file = 'no_file'
+
+config.status.file_uploaded = 'file_uploaded'
+config.status.file_deleted = 'file_deleted'
+
+module.exports = config;
